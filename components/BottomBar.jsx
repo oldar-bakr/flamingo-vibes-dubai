6313533c42daef5dac205ddb43a9b8246f6eef75
oldarbakr <bakr.oldar@std.izu.edu.tr>
2024-04-17 18:07:00 +0300

    update10

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const BottomBar = () => {
   return (
-    <div className="bg-dark1 text-white p-4 fixed bottom-0 w-full z-50 flex justify-center items-center ">
-            <button className="                rounded-2xl bg-orange1 text-white px-[30px] py-[10px] text-[2rem] font-bold scale1">BOOK NOW</button>
+    <div className="bg-dark1 text-white p-4 fixed bottom-0 w-full z-50 flex justify-center items-center">
+      <button className="rounded-2xl bg-orange1 text-white px-4 md:px-8 py-2 md:py-4 text-lg md:text-2xl font-bold scale1">
+        BOOK NOW
+      </button>
     </div>
   );
 };
