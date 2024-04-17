e3f286d6bb2b785ef8a433b1074bfe07ee37b059
oldarbakr <bakr.oldar@std.izu.edu.tr>
2024-04-17 19:08:24 +0300

    update14

--- components/Section6.jsx
+++ components/Section6.jsx
@@ -11,7 +11,7 @@ const Section6 = () => {
             </h2>
             <p className="text-lg mb-2 text-white py-4 leading-normal">
               Dubai is a mix of cultures and food choices. You can enjoy
-              traditional Emirati dishes or try international cuisine, there's a
+              traditional Emirati dishes or try international cuisine, theres a
               delicious bite for everyone!
             </p>
             <a
